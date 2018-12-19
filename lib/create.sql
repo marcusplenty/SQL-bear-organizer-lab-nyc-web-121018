12528315909210ee41aa38d5e07e415aa1262c13
Marcus Plenty <marcusplenty@gmail.com>
2018-12-19 15:45:35 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,10 +1,8 @@
 CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
-  name TEXT,
-  age INTEGER,
-  gender TEXT,
-  color TEXT,
-  temperament TEXT,
-  alive BOOLEAN,
-
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    color TEXT,
+    temperament TEXT
 );
